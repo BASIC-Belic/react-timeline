242c37714d7e5dab1c509af12a035502c1d8ee96
Chantelle Belic <chantelle.belic@gmail.com>
2018-11-28 12:02:51 -0800

    refactor timeline.js with class component

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
+import PropTypes from 'prop-types';
 
-const Timeline = (props) => {
-  // Fill in your code here
+class Timeline extends React.Component {
 
-  const parseData = props.events.map( (event, i) => {
-    return <TimelineEvent key={i} person={event.person} status={event.status} time={event.timeStamp}/>
-  });
+  constructor(props) {
+    super(props);
+  }
+  getEventsData() {
+    const eventsData = this.props.events.map( (event, i) => {
+      return <TimelineEvent key={i} person={event.person} status={event.status} time={event.timeStamp}/>
+    });
+    return eventsData;
+  }
+  render() {
+    const eventsData = this.getEventsData();
 
-  return (
-    <section className="timeline">
-      {parseData}
-    </section>
-  );
+    return (
+      <section className="timeline">
+        {eventsData}
+      </section>
+    );
+  }
 }
+
+Timeline.propTypes = {
+  events: PropTypes.array.isRequired
+};
 
 export default Timeline;
